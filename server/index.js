1cb7a66d244efd1bad23677cb1c6f01915fd3f28
apreswilson <apreswilson@gmail.com>
2024-12-08 16:59:07 -0500

    Deploy fix 2

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ import cors from "cors";
 import express from "express";
 import router from "./routes/todoroutes.js"
 import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-
 
 await mongoose.connect(process.env.MONGODB_URI, {
    bufferCommands: false
@@ -16,7 +19,9 @@ await mongoose.connect(process.env.MONGODB_URI, {
 
 app.use("/todo", router);
 
-app.use(express.static(path.join(__dirname, "app/dist")));
+console.log(__dirname);
+
+app.use(express.static(path.join(__dirname, "../app/dist")));
 
 app.get("*", (req, res) => {
    res.sendFile(path.join(__dirname, "app/dist", "index.html"));
